0cf62e4f2a61088538b795813812e88c2a56233f
chihabreact <bouzar866@gmail.com>
2023-07-26 16:08:17 +0000

    simple add to review section on the app.js
    to complete the stracture (dont worry about its not a big change)

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "./App.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import {
+  AboutUS,
+  Blog,
+  ContactUS,
+  Features,
+  Footer,
+  Header,
+  LandingPage,
+  OurClinets,
+  Products,
+  Reviews,
+} from "./containers";
+import { NavigationBar } from "./components";
+import { Container } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <NavigationBar></NavigationBar>
+        <LandingPage></LandingPage>
+        <AboutUS></AboutUS>
+        <Products></Products>
+        <OurClinets></OurClinets>
+        <Reviews></Reviews>
+        <ContactUS></ContactUS>
+        <Footer></Footer>
+      </Container>
+    </div>
+  );
+}
+
+export default App;

d7cd4cda304976b951cce7ba050e99fc064f5b3f
chihabreact <bouzar866@gmail.com>
2023-07-26 17:29:31 +0000

    our clients section almost finished
    it still needs to be responsive and to get the full width of the body

---
+++ src/assets/logos/index.js
@@ -0,0 +1,5 @@
+export { default as logo } from "./logo.png";
+export { default as logo1 } from "./1logo.png";
+export { default as logo2 } from "./2logo.png";
+export { default as logo3 } from "./3logo.png";
+export { default as logo4 } from "./4logo.png";

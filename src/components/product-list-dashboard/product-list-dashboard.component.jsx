cae294cd6c228baaf9e0f0d517a116182a8a51bf
Soheyb Zerari <soheyb.zerari@univ-constantine2.dz>
2023-07-28 00:41:04 +0100

    update the design of the product table

--- src/components/product-list-dashboard/product-list-dashboard.component.jsx
+++ src/components/product-list-dashboard/product-list-dashboard.component.jsx
@@ -37,11 +37,11 @@ const ProductListDashboard = () => {
         <Table sx={{ minWidth: 250 }} aria-label="simple table">
             <TableHead>
             <TableRow className="header-row">
-                <TableCell align="center">Image</TableCell>
-                <TableCell align="center">Name</TableCell>
-                <TableCell align="center">Price</TableCell>
-                <TableCell align="center">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="center">Action</TableCell>
+                <TableCell sx={{ padding: 1 }} align="center">Image</TableCell>
+                <TableCell sx={{ padding: 1 }} align="center">Name</TableCell>
+                <TableCell sx={{ padding: 1 }} align="center">Price</TableCell>
+                <TableCell sx={{ padding: 1 }} align="center">Carbs&nbsp;(g)</TableCell>
+                <TableCell sx={{ padding: 1 }} align="center">Action</TableCell>
             </TableRow>
             </TableHead>
             <TableBody>
@@ -50,13 +50,13 @@ const ProductListDashboard = () => {
                 key={row.name}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                 >
-                <TableCell align="center" component="th" scope="row">
+                <TableCell sx={{ padding: 1 }} align="center" component="th" scope="row">
                     <img className="product-image" src={photo} alt="logo"/>
                 </TableCell>
-                <TableCell align="center">{row.calories}</TableCell>
-                <TableCell align="center">{row.fat}</TableCell>
-                <TableCell align="center">{row.carbs}</TableCell>
-                <TableCell align="center">
+                <TableCell sx={{ padding: 1 }} align="center">{row.calories}</TableCell>
+                <TableCell sx={{ padding: 1 }} align="center">{row.fat}</TableCell>
+                <TableCell sx={{ padding: 1 }} align="center">{row.carbs}</TableCell>
+                <TableCell sx={{ padding: 1 }} align="center">
                     <IconButton aria-label="delete" size="large">
                         <DeleteIcon fontSize="inherit" className="delete-icon" />
                     </IconButton>

020c4e7637c2aee5c680b374de68db1651de3785
Soheyb Zerari <soheyb.zerari@univ-constantine2.dz>
2023-07-28 00:04:11 +0100

    initial dashboard design

---
+++ src/components/product-list-dashboard/product-list-dashboard.component.jsx
@@ -0,0 +1,72 @@
+import "./product-list-dashboard.styles.css"
+
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from '@mui/icons-material/Delete';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import logo from "../../assets/logo.jpg"
+import photo from "../../assets/landcruiser.jpg"
+
+function createData(
+    name,
+    calories,
+    fat,
+    carbs,
+    protein
+  ) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+const ProductListDashboard = () => {
+  return (
+    <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+            <TableRow className="header-row">
+                <TableCell >Image</TableCell>
+                <TableCell align="center">Name</TableCell>
+                <TableCell align="center">Price</TableCell>
+                <TableCell align="center">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="center">Action</TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {rows.map((row) => (
+                <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                <TableCell component="th" scope="row">
+                    <img className="product-image" src={photo} alt="logo"/>
+                </TableCell>
+                <TableCell align="center">{row.calories}</TableCell>
+                <TableCell align="center">{row.fat}</TableCell>
+                <TableCell align="center">{row.carbs}</TableCell>
+                <TableCell align="center">
+                    <IconButton aria-label="delete" size="large">
+                        <DeleteIcon fontSize="inherit" className="delete-icon" />
+                    </IconButton>
+                </TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+        </TableContainer>
+  )
+}
+
+export default ProductListDashboard
---
+++ src/routes/dashboard/dashboard.component.jsx
@@ -0,0 +1,48 @@
+import "./dashboard.styles.css"
+
+import ProductListDashboard from "../../components/product-list-dashboard/product-list-dashboard.component"
+
+import logo from "../../assets/logo.jpg"
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Button from "@mui/material/Button";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+        <div className="responsive-container">
+            <div className="responsive">
+                <nav className="dashboard-nav-bar">
+                    <div className="nav-bar-left">
+                        <img className="logo-image-dashboard" src={logo} alt="LOGO" />
+                        <div className="nav-link">Dashboard</div>
+                        <div className="nav-link">Products</div>
+                    </div>
+                    <div className="nav-bar-right">
+                        <div>Admin</div>
+                        <AccountCircleIcon />
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div className="responsive-container">
+            <div className="responsive">
+                <div className="header-container">
+                    <div className="left-section-header">
+                        <h3>Products</h3>
+                        <p className="header-paragraph">A list of all the Products</p>
+                    </div>
+                    <div className="right-section-header">
+                        <Button variant="contained" style={{backgroundColor: "#00B300"}} className="add-button">Add Product</Button>
+                    </div>
+                </div>
+                <div className="product-table-container">
+                    <ProductListDashboard />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard

5817ec54cc6f55f20f6f4dd3bf35a8579fbffebf
Soheyb Zerari <soheyb.zerari@univ-constantine2.dz>
2023-08-03 21:04:14 +0200

    update the style of product page and add navigation bar to it

--- src/routes/Products/ProductsPage.jsx
+++ src/routes/Products/ProductsPage.jsx
@@ -5,6 +5,7 @@ import { Container, Stack } from "@mui/system";
 import { Button, Typography } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import EquipmentList from "../../components/EquipmentList/EquipmentList";
+import NavigationBar from "../../components/NavigationBar/navigation-bar.component";
 const ProductsPage = () => {
   const theme = createTheme({
     typography: {
@@ -32,29 +33,32 @@ const ProductsPage = () => {
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <Stack>
-        <Container>
-          <Stack>
-            <Typography variant="h2" align="center" paddingY="2rem">
-              Find the Perfect Equipment for Your Project
-            </Typography>
-            <Typography color="secondary">
-              we take pride in providing our customers with the best tools and
-              machinery for any project they undertake. From heavy-duty trucks
-              to versatile generators, we have everything you need to ensure
-              your project runs smoothly and efficiently. Our selection of
-              equipment for sale and rent is carefully curated to ensure that
-              you get the highest quality and most reliable tools at the best
-              value. Explore our inventory today and find the perfect equipment
-              for your next project!
-            </Typography>
-            <Button fullWidth="false">GET STARTED</Button>
-          </Stack>
-          <EquipmentList></EquipmentList>
-        </Container>
-      </Stack>
-    </ThemeProvider>
+    <div className="products-page-container">
+      <NavigationBar />
+      <ThemeProvider theme={theme}>
+        <Stack>
+          <Container>
+            <Stack>
+              <Typography variant="h2" align="center" paddingY="2rem">
+                Find the Perfect Equipment for Your Project
+              </Typography>
+              <Typography color="secondary">
+                we take pride in providing our customers with the best tools and
+                machinery for any project they undertake. From heavy-duty trucks
+                to versatile generators, we have everything you need to ensure
+                your project runs smoothly and efficiently. Our selection of
+                equipment for sale and rent is carefully curated to ensure that
+                you get the highest quality and most reliable tools at the best
+                value. Explore our inventory today and find the perfect equipment
+                for your next project!
+              </Typography>
+              <Button fullWidth="false">GET STARTED</Button>
+            </Stack>
+            <EquipmentList></EquipmentList>
+          </Container>
+        </Stack>
+      </ThemeProvider>
+    </div>
   );
 };
 

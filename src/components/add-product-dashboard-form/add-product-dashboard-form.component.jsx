c0395517da71c5ef191c9d47339fdb8d476971de
Soheyb Zerari <soheyb.zerari@univ-constantine2.dz>
2023-08-16 22:22:57 +0200

    add the redux and state to add product form and add success text when product added

--- src/components/add-product-dashboard-form/add-product-dashboard-form.component.jsx
+++ src/components/add-product-dashboard-form/add-product-dashboard-form.component.jsx
@@ -7,8 +7,14 @@ import TextArea from "../textarea/textarea.component"
 
 import Button from "@mui/material/Button"
 import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
 
-import { addProduct } from "../../utils/request"
+import { setProducts } from "../../store/products/products.action"
+
+import { 
+  addProduct,
+  getProducts 
+} from "../../utils/request"
 
 const defaultTextFormFields = {
     title: "",
@@ -16,9 +22,9 @@ const defaultTextFormFields = {
 }
 
 const defaultImagesFormFields = {
-    image1: "",
-    image2: "",
-    image3: ""
+    image1: null,
+    image2: null,
+    image3: null
 }
 
 const AddProductDashboardForm = () => {
@@ -26,6 +32,7 @@ const AddProductDashboardForm = () => {
   const [textFormFields, setTextFormFields] = useState(defaultTextFormFields);
   const [imagesFormFields, setImagesFormFields] = useState(defaultImagesFormFields);
   const [productFromCredentials, setProductFromCredentials] = useState(true);
+  const [successFormSubmission, setSuccessFormSubmission] = useState(false);
 
   const { 
     title, 
@@ -39,6 +46,7 @@ const AddProductDashboardForm = () => {
   } = imagesFormFields;
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const onChangeHandler = (event) => {
     const { name, value } = event.target;
@@ -47,22 +55,42 @@ const AddProductDashboardForm = () => {
 
   const onImageChangeHandler = (event) => {
     const selectedImage = event.target.files[0];
-    console.log(event.target.name);
-    console.log(event.target.files[0]);
+    event.target.value = null;
     setImagesFormFields({...imagesFormFields, [event.target.name]: selectedImage});
+  }
+
+  const updateProductsState = async () => {
+    const productsResponse = await getProducts();
+    if (!productsResponse) {
+      console.error("something went wrong");
+    } else {
+      dispatch(setProducts(productsResponse));
+    }
   }
 
   const onAddProductHandler = async () => {
     const productResponse = await addProduct(title, description, image1, image2, image3);
 
+    console.log("everything setted correctly");
     if (!productResponse) {
       setProductFromCredentials(false);
       console.error("something went wrong");
     } else {
       if (!productFromCredentials) setProductFromCredentials(true);
+
+      setSuccessFormSubmission(true);
       setTextFormFields(defaultTextFormFields);
       setImagesFormFields(defaultImagesFormFields);
-      navigate("/dashboard/products");
+
+      await updateProductsState();
+
+      const timeoutId = setTimeout(() => {
+        setSuccessFormSubmission(false);
+      }, 2000);
+    
+      return () => {
+        clearTimeout(timeoutId);
+      };
     }
   }
 
@@ -76,7 +104,8 @@ const AddProductDashboardForm = () => {
             <h3>Product Infomation</h3>
             <p className="header-paragraph">Add new product</p>
         </div>
-        <p className={`add-product-form-credentials ${!productFromCredentials && ".add-product-form-wrong-credentials"}`}>Wrong Credentials</p>
+        <p className={`add-product-form-credentials ${!productFromCredentials && "add-product-form-wrong-credentials"}`}>Wrong Credentials</p>
+        <p className={`add-product-form-credentials ${successFormSubmission && "successful-product-form-submission"}`}>Product added successfully</p>
         <div>
             <div className="top-input-container">
                 <Input
